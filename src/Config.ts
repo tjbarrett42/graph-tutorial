24533489d7d36e44bf7710979ccb1b145bfff718
tjbarrett42 <tjbarrett42@gmail.com>
2021-02-10 09:50:31 -0700

    Azure link/setup

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '7cf69655-e93f-4ab9-b44a-fc604f7c7508',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read',
+        'mailboxsettings.read',
+        'calendars.readwrite'
+    ]
+};
